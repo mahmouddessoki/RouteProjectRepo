1338ca85a864198800b1193346b5920cad4fcfe7
Amed <mahmouddessoki33@gmail.com>
2020-12-27 03:28:22 +0200

    groupProject

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function test()
+{
+    document.getElementById("one").style.top="0";
+}
+function test1()
+{
+    document.getElementById("two").style.top="0";
+}
+function test2()
+{
+    document.getElementById("three").style.top="0";
+}
+
+function t()
+{
+  x="ahmed"
+}
+console.log(x);
